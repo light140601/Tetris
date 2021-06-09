8f0ed76da62ff2138199a35a7de70eae3adf9251
DoanTranPhiKhanh-19130097 <82622093+DoanTranPhiKhanh-19130097@users.noreply.github.com>
2021-06-09 12:51:34 +0700

    game-update

--- src/model/Game.java
+++ src/model/Game.java
@@ -7,10 +7,16 @@ import factory.Shape;
 import factory.ShapeRandomFactory;
 
 public class Game extends Observable implements IGame {
+	// game là model trong mvc, là observable của view
+
 	private int[][] board;
 	private int tileSize; // kich thuoc tung khoi
-	private boolean pause;
 	private boolean gameOver;
+	private Shape currentShape;
+	private boolean running = true;
+	private boolean pause = false;
+	private final Object pauseLock = new Object(); // pause
+
 
 	public Game(int[][] board, int tileSize, boolean pause, boolean gameOver) {
 		this.board = board;
@@ -20,45 +26,68 @@ public class Game extends Observable implements IGame {
 	}
 
 	public ShapeRandomFactory shapeRandomFactory() {
-		// insert code
-		return null;
+		return new ShapeRandomFactory();
 	}
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
-		System.out.println(123);
-		
+		currentShape = this.shapeRandomFactory().creatShape('?', tileSize, this);		
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-		
+		currentShape.update();
+		// update cho observer
+		super.setChanged();
+		super.notifyObservers();
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		// TODO Auto-generated method stub
-		
+		while (running) {
+			update();
+			try {
+				// draw board
+				for (int row = 0; row < board.length; row++) {
+					for (int col = 0; col < board[col].length; col++) {
+						g.drawRect(row * tileSize, col * tileSize, tileSize, tileSize); // vẽ ô vuông
+					}
+				}
+
+				// pause game
+				if (pause) {
+					try {
+						pauseLock.wait();
+					} catch (InterruptedException ex) {
+						System.out.println("bug");
+					}
+				}
+
+				// draw shape
+				currentShape.draw(g);
+				Thread.sleep(500);
+
+				// ........
+			} catch (InterruptedException e) {
+				System.out.println("bug");
+			}
+		}
 	}
 
 	@Override
 	public void setNextShape() {
-		// TODO Auto-generated method stub
-		
+		currentShape = shapeRandomFactory().creatShape('?', tileSize, this);
+	
 	}
 
 	@Override
 	public void setCurrentShape() {
-		// TODO Auto-generated method stub
-		
+		// ...
 	}
 
 	@Override
 	public void checkLine() {
-		// TODO Auto-generated method stub
-		
+		// ...
 	}
 
 	@Override
@@ -68,6 +97,10 @@ public class Game extends Observable implements IGame {
 
 	@Override
 	public Shape getCurrentShape() {
-		return null;
+		return this.currentShape;
+	}
+	
+	public void pause() {
+		pause=!pause;
 	}
 }
