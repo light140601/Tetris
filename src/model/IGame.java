e1cad6b36e140d41585afba66a3795470f5269fe
DoanTranPhiKhanh-19130097 <82622093+DoanTranPhiKhanh-19130097@users.noreply.github.com>
2021-06-11 14:35:12 +0700

    6-11

--- src/model/IGame.java
+++ src/model/IGame.java
@@ -6,18 +6,27 @@ import factory.Shape;
 
 public interface IGame {
 	public void init();
+
 	public void update();
+
 	public void draw(Graphics g);
 
 	public Shape getNextShape();
 	public void setNextShape();
 	public void setCurrentShape();
-
 	public Shape getCurrentShape();
 	public void checkLine();
 
-
 	public int[][] getBoard();
 	public int getTileSize();
+
+	public void left();
+	public void right();
+	public void down();
+	public void setNormalSpeed();
+	public void rotate();
+	public void pause();
+
+	public void resume();
 
 }
